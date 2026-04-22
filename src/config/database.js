2da9c65eba73a3afec63291929b1453fe4364309
mohd-Shahid <ansarishahidsalim786@gmail.com>
2026-04-22 09:53:58 +0530

    changing database

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/ecommerce_skin';
+const MONGODB_URI = process.env.MONGODB_URI;
 
 const connectDatabase = async () => {
   try {
